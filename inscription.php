a051c15151b8b5b6e0ff2b0c7cbbf62a3b386b73
MathieuPignon <pignonmathieu@hotmail.fr>
2015-11-12 21:12:48 +0100

    Page inscription
    récupération d'un formulaire et affiche un message

--- inscription.php
+++ inscription.php
@@ -1,8 +1,10 @@
-<?php
+﻿<?php
 
 require_once "inc/config.inc.php";
 session_start();
 
 /**
- * Récupère les données d'une formulaire d'inscription, les traite puis affiche un message correspondant
+ * Récupère les données d'un formulaire d'inscription, les traite puis affiche un message correspondant
  */
+$gest = new Gestionnaire ();
+echo $gest->inscription($_POST);
